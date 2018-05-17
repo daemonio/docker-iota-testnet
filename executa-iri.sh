609559ba8d2dc0d41b373341dc15611fc773efcd
daemonio <undefinido@gmail.com>
2018-05-17 15:18:53 -0300

    Update executa-iri.sh

--- executa-iri.sh
+++ executa-iri.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-nohup java -jar iri-1.4.2.1.jar -p 14265 --testnet --remote &
+sh verifica-iri.sh &
 
-PORT=14265
-TRIALS=60
-
-while ! lsof -i :$PORT
-do
-    [ $TRIALS -le 0 ] && exit -1
-
-    sleep 5
-
-    let TRIALS--
-done
-
-java -jar iota-testnet-tools-0.1-SNAPSHOT-jar-with-dependencies.jar Coordinator localhost 14700
+java -jar iri-1.4.2.1.jar -p 14265 --testnet --remote
